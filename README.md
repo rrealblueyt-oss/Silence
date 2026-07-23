3be99211698dfee4137894924070db23c4cb0aec
RealblueYT <rrealblueyt@gmail.com>
2026-07-23 23:03:35 +0700

    Update and rename README.md to Silence-entities20%25.lua

--- README.md
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/NTTTTTTTT/upload/refs/heads/main/content/0874f982896545fd92059cdcd052ea57_obf.lua",true))()
---
+++ Silence-entities20%25.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NTTTTTTTT/upload/refs/heads/main/content/0d59e159f195406daf0812e69a438697_obf.lua",true))()
